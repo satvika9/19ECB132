109994e454da0d46d146d41c578a13609a235fb6
VIGINIGIRI SATVIKA VARMA SATVIKA <101540366+satvika9@users.noreply.github.com>
2022-06-02 16:56:14 +0000

    output

--- lettercount.c
+++ lettercount.c
@@ -11,20 +11,9 @@ int main(void) {
   printf("\nThe number of letters in the given sentence are : %d\n", number_of_letters);
   return 0;
 }
+/*
+output:
+Enter a sentence: pyro
 
-/* Obervations:
-01. How to work with sentences having a space in between?
-
-Output:
-Case - 01
-Enter a sentence: suraj
-The number of letters in the given sentence are : 5
-
-Case - 02  
-Enter a sentence: suraj aravind
-The number of letters in the given sentence are : 5
-
-*/  
-
-
-  
+The number of letters in the given sentence are : 4
+*/
